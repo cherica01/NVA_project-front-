f692661dbf7b348507a164742bda226cc497987a
Cherica <riantsoacherica01@gmail.com>
2025-04-07 11:24:46 +0300

    MAMENO RPEO

--- nva/app/page.tsx
+++ nva/app/page.tsx
@@ -162,7 +162,6 @@ export default function Login() {
               }}
             />
           ))}
-
                 style={{
                   left: `${Math.random() * 100}%`,
                 }}
