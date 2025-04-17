2ee3d8f60d7284b24da251309ad1002f87db01fe
Cherica <riantsoacherica01@gmail.com>
2025-04-17 20:48:48 +0300

    elaela

--- nva/app/agent/agenda/page.tsx
+++ nva/app/agent/agenda/page.tsx
@@ -55,6 +55,7 @@ export default function AgentAgendaPage() {
 
   useEffect(() => {
         setSuccess(null)
+    }
 
   const fetchMonthData = async (year: number, month: number) => {
     setLoading(true)
