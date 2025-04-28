54f4c12d32814d163f100005592163603824c256
Cherica <riantsoacherica01@gmail.com>
2025-04-28 20:42:24 +0300

    build

--- nva/next.config.ts
+++ nva/next.config.ts
@@ -2,17 +2,26 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["picsum.photos"],
+    remotePatterns: [
+      {
+        protocol: "http",
         hostname: "127.0.0.1",
         port: "8000",
         pathname: "/media/**", // Autorise les images dans le dossier /media/
       },
+      {
+        protocol: "https",
         hostname: "tile.openstreetmap.org", // Pour les cartes OpenStreetMap utilisées dans le projet
         port: "",
         pathname: "/**",
       },
+      {
         protocol: "https",
+        hostname: "picsum.photos", // Gardez ceci si vous utilisez des images de picsum.photos ailleurs
+        port: "",
         pathname: "/**",
+      },
+    ],
   },
 };
 
