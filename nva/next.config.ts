4f50f18feb579af08d08a99b16eb988fdc3e0b56
Cherica <riantsoacherica01@gmail.com>
2025-04-28 20:41:19 +0300

    build

--- nva/next.config.ts
+++ nva/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: ["picsum.photos"],
+        hostname: "127.0.0.1",
         pathname: "/media/**", // Autorise les images dans le dossier /media/
       },
   },
