64f54b8c89ff6f7a72612b1c6ea51ad3937e5a18
Cherica <riantsoacherica01@gmail.com>
2025-04-28 20:42:13 +0300

    build

--- nva/next.config.ts
+++ nva/next.config.ts
@@ -7,6 +7,7 @@ const nextConfig: NextConfig = {
         port: "8000",
         pathname: "/media/**", // Autorise les images dans le dossier /media/
       },
+        hostname: "tile.openstreetmap.org", // Pour les cartes OpenStreetMap utilisées dans le projet
         port: "",
         pathname: "/**",
       },
