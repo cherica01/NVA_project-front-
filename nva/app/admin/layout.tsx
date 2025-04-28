379c3e2531c6494a225188c90bf387bdd03e8c08
Cherica <riantsoacherica01@gmail.com>
2025-04-28 20:43:24 +0300

    build

--- nva/app/admin/layout.tsx
+++ nva/app/admin/layout.tsx
@@ -1,5 +1,5 @@
 import AdminNavbar from "@/app/components/AdminNavbar";
-
+import Chatbot from "@/components/Chatbot"
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
     return (
         <div className="min-h-screen bg-gradient-to-br from-orange-50 to-red-100 dark:from-gray-900 dark:to-gray-800 relative">
