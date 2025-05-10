23b515cc51af63719fc5aebb2d2d26ad2d9049fc
Cherica <riantsoacherica01@gmail.com>
2025-05-10 21:35:27 +0300

    3-J

--- nva/app/agent/layout.tsx
+++ nva/app/agent/layout.tsx
@@ -10,7 +10,7 @@ export default function AgentLayout({ children }: { children: React.ReactNode })
   return (
     <div className="flex h-screen bg-gray-100 dark:bg-gray-900">
       <AgentNavbar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-      <div className="flex-1 flex flex-col overflow-hidden lg:pl-64">
+        </div>
         <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-100 dark:bg-gray-900">
           <div className="container mx-auto px-6 py-8">{children}</div>
         </main>
