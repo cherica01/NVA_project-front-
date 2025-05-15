55b359670118e2f144a9e1e036dac1cde325e3d5
Cherica <riantsoacherica01@gmail.com>
2025-05-15 19:19:09 +0300

    mameno

--- nva/app/agent/layout.tsx
+++ nva/app/agent/layout.tsx
@@ -12,7 +12,7 @@ export default function AgentLayout({ children }: { children: React.ReactNode })
       <AgentNavbar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
         </div>
         <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-100 dark:bg-gray-900">
-          <div className="container mx-auto px-6 py-8">{children}</div>
+}
         </main>
         <Chatbot userRole="admin" title="Assistant AGENT" />
       </div>
